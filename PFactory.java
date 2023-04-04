388c8f0a73a7d04919a56d70ebcb03e1532ed533
JoseAngelMorales <105880422+JoseAngelMorales@users.noreply.github.com>
2023-04-03 23:45:27 -0600

    Add files via upload

--- PFactory.java
+++ PFactory.java
@@ -5,12 +5,12 @@ public class PFactory extends Namer{
 
         String nice;
 
-        //Busca la primera coma y guarda desde el inicio hasta la coma para la palabra en ingles
+        //Busca la primera coma y guarda desde el inicio hasta la coma para el nombre del proceso
         int i = s.indexOf(",");
         nombrep = s.substring(0, i).trim();
         s = s.replaceFirst(s.substring(0, i+1).trim(), "");
 
-        //Busca para la segunda coma y guarda desde el nuevo inicio para la palabra en ingles y el resto para frances
+        //Busca para la segunda coma y guarda desde el nuevo inicio para el nombre del usuario y el valor nice
         i = s.indexOf(",");
         nombreu = s.substring(0, i);
         nice = s.replaceFirst(s.substring(0, i+1).trim(), "");
